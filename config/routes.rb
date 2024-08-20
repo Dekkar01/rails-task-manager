c8c9f313b4a599aca248323edace43ba2eedccdd
Anastasiia Lykhvar <alikhvar2001@gmail.com>
2024-08-20 15:52:43 +0200

    press f for me

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,23 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  # get keyword will "get " HTML page so GET request
+  # post keyword will post something to DB , POST request
+  # patch keyword will update data in DB
+  # delete keyword will delete something in db
+
+  root 'tasks#index'
+
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+  get  'tasks/:id/edit', to: 'tasks#edit', as: :update_task
+  patch 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id/', to: 'tasks#destroy', as: :delete_task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+
 end
